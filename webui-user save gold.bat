babc943edf0bbf3e7591d9135409e9626747cb11
Locke Chen <locke12456@gmail.com>
2023-05-31 10:57:53 +0800

    add my settings.

---
+++ webui-user save gold.bat
@@ -0,0 +1,9 @@
+@echo off
+set SAV_OPT=--precision full --no-half-vae 
+set PYTHON=
+set GIT=
+set VENV_DIR=venv
+set COMMANDLINE_ARGS= --medvram --xformers --force-enable-xformers --always-batch-cond-uncond --opt-channelslast --no-hashing --disable-nan-check --api --xformers-flash-attention --opt-split-attention --no-half-vae
+set PYTORCH_CUDA_ALLOC_CONF=garbage_collection_threshold:0.6, max_split_size_mb:32
+
+call webui.bat
